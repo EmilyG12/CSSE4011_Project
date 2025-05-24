172657c3e92a1a8cc176e9e1dc8b75e113dea94a
EmilyG12 <140981674+EmilyG12@users.noreply.github.com>
2025-05-25 09:40:03 +1000

    Modified ultrasonic.h to include second sensor

--- libs/ultrasonic.h
+++ libs/ultrasonic.h
@@ -6,8 +6,8 @@
 #define ULTRASONIC_H
 #include <stdint.h>
 
-int init_ultrasonic(void(*callback)(uint8_t metres, uint8_t cms));
-uint8_t get_ultrasonic_metres(void);
-uint8_t get_ultrasonic_cms(void);
+int init_ultrasonic(int id, void(*callback)(int id, uint8_t metres, uint8_t cms));
+uint8_t get_ultrasonic_metres(int id);
+uint8_t get_ultrasonic_cms(int id);
 
 #endif //ULTRASONIC_H
